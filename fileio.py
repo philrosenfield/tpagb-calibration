a73e58c3b91cd2972276a32f9527cfea503b9f72
Phil Rosenfield <philrosenfield@gmail.com>
2015-03-12 18:22:15 +0100

    fixed plotting, added compare_to_gal to analyze

---
+++ fileio.py
@@ -0,0 +1,13 @@
+import ResolvedStellarPops as rsp
+
+def load_obs(target, optfilter1=''):
+    """load in NIR and OPT galaxy as StarPop objects"""
+    from astropy.io import fits
+    nirgalname, = rsp.fileio.get_files(data_loc, '*{}*fits'.format(target.upper()))
+    optgalname, = rsp.fileio.get_files(data_loc, ('*{}*{}*fits'.format(target, optfilter1).lower()))
+    nirgal = rsp.StarPop()
+    nirgal.data = fits.getdata(nirgalname)
+
+    optgal = rsp.StarPop()
+    optgal.data = fits.getdata(optgalname)
+    return optgal, nirgal
