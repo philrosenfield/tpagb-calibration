21275413c3cff28028ec362804b24c12b69cc39b
Phil Rosenfield <philrosenfield@gmail.com>
2015-12-22 14:32:19 -0500

    minor

--- scripts/plotting.sh
+++ scripts/plotting.sh
@@ -9,7 +9,8 @@ $call $LOC/${1}_f110w_f160w.fits out_${1}*_???.dat
 }
 
 function runit {
-call="python -m tpagb_calibration.plotting.plotting -b 0.9 $2 $3 $4 $5 $6"
+call="python -m tpagb_calibration.plotting.plotting $2 $3 $4 $5 $6"
+#call="python -m tpagb_calibration.plotting.plotting -b 0.9 $2 $3 $4 $5 $6"
 target=$(echo $1 | cut -d '_' -f 1)
 lf=$(ls ${target}*lf.dat)
 $call $LOC/${1}_f110w_f160w.fits $lf
@@ -20,26 +21,26 @@ $call $LOC/${1}_f110w_f160w.fits $lf
 #
 #normit ddo71_f606w_f814w -c 1.37,0.40
 #normit ddo78_f475w_f814w -c 1.55,0.40
-#normit ddo82_f606w_f814w -c 1.59,0.40
-#normit eso540-030_f606w_f814w -c 1.27,0.40
+normit ddo82_f606w_f814w -c 1.59,0.40
+normit eso540-030_f606w_f814w -c 1.27,0.40
 #normit hs117_f606w_f814w -c 1.32,0.40
-#normit kdg73_f475w_f814w -c 1.33,0.40
+normit kdg73_f475w_f814w -c 1.33,0.40
 #normit kkh37_f475w_f814w -c 1.38,0.40
 #normit m81-deep_f475w_f814w -c 1.80,0.60
 #normit m81-deep_f606w_f814w -c 1.80,0.60
-#normit ngc2403-deep_f606w_f814w -c 1.62,0.60 -q MAG2_WFPC2,MAG4_IR
-#normit ngc2403-halo-6_f606w_f814w -c 1.59,0.60
+normit ngc2403-deep_f606w_f814w -c 1.62,0.60 -q MAG2_WFPC2,MAG4_IR
+normit ngc2403-halo-6_f606w_f814w -c 1.59,0.60
 #normit ngc2976-deep_f606w_f814w -c 1.69,0.40
-#normit ngc300-wide1_f475w_f814w -c 1.74,0.60
-#normit ngc300-wide1_f606w_f814w -c 1.74,0.60
-#normit ngc3741_f475w_f814w -c 1.34,0.40
+normit ngc300-wide1_f475w_f814w -c 1.74,0.60
+normit ngc300-wide1_f606w_f814w -c 1.74,0.60
+normit ngc3741_f475w_f814w -c 1.34,0.40
 #normit ngc404-deep_f606w_f814w -c 1.74,0.40 -q MAG2_WFPC2,MAG4_IR
 #normit ngc4163_f475w_f814w -c 1.49,0.40
-#normit ngc4163_f606w_f814w -c 1.49,0.40
+normit ngc4163_f606w_f814w -c 1.49,0.40
 #normit scl-de1_f606w_f814w -c 1.32,0.40
-#normit ugc4305-1_f555w_f814w -c 1.47,0.40
-#normit ugc4305-2_f555w_f814w -c 1.46,0.40
-#normit ugc4459_f555w_f814w -c 1.40,0.40
+normit ugc4305-1_f555w_f814w -c 1.47,0.40
+normit ugc4305-2_f555w_f814w -c 1.46,0.40
+normit ugc4459_f555w_f814w -c 1.40,0.40
 normit ugc5139_f555w_f814w -c 1.37,0.40
 normit ugc8508_f475w_f814w -c 1.41,0.40
 normit ugca292_f475w_f814w -c 1.15,0.40
