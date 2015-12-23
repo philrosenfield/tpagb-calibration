c9a876ee66ed0db5cce1edc98fb1030f49f8e783
Phil Rosenfield <philrosenfield@gmail.com>
2015-12-23 12:08:01 -0500

    added systematic errors from weisz2011 to csfr plot

--- plotting/csfr_plots.py
+++ plotting/csfr_plots.py
@@ -76,7 +76,7 @@ def default_run():
         ax = sfh.plot_csfr(ax=ax)
         
         try:
-            err_file, =  get_files(err_loc, '{}*fine'.format(targets[i].replace('gc','').replace('-','_')))
+            err_file, = get_files(err_loc, '{}*fine'.format(targets[i]))
             esfh = SFH(err_file, meta_file=meta_file)
             esfh.plot_csfr(ax=ax, fill_between_kw={'alpha':0.25}, data=False)
         except:
