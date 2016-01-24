816f213a5021e75700facc5677175745797c3aed
Phil Rosenfield <philrosenfield@gmail.com>
2016-01-24 12:50:12 -0500

    almost added 4...

--- plotting/animate_cmd.py
+++ plotting/animate_cmd.py
@@ -24,44 +24,48 @@ def read_data(infile, lffile):
     return SimGalaxy(infile), load_lf_file(lffile)
 
 
-def load_data(sgal, filter1, filter2, lfd=None, zcol='logAge'):
+def load_data(sgal, filter1, filter2, lfd=None):
     """load lage, mass, color, mag and slice by inx_norm from lfd"""
     if lfd is None:
-        sinds = np.arange(sgal.data[zcol])
+        sinds = np.arange(sgal.data['logAge'])
     else:
         sinds = lfd['idx_norm'][0]
-    zdata = sgal.data[zcol][sinds]
+    lage = sgal.data['logAge'][sinds]
     color = sgal.data[filter1][sinds] - sgal.data[filter2][sinds]
     mag = sgal.data[filter2][sinds]
     mass = sgal.data['m_ini'][sinds]
-    return zdata, mass, color, mag
+    stage = sgal.data['stage'][sinds]
+    return lage, mass, color, mag, stage
 
-def plot_setup(filter1, filter2, fsize=20, xlabel=None):
+def plot_setup(filter1, filter2, fsize=20):
     """Setup subplots, axis limits hard coded... """
-    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(6, 8))
-
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 16))
+    axs = np.concatenate(axs)
     plt.subplots_adjust(left=0.2, hspace=0.25)
-    [ax.tick_params(labelsize=18) for ax in (ax1, ax2)]
+    [ax.tick_params(labelsize=18) for ax in axs]
 
-    ax1.set_xlim(-0.5, 3)
-    ax1.set_ylim(28.5, 16)
+    for ax in [axs[0], axs[1]]:
+        ax.set_xlim(-0.5, 3)
+        ax.set_ylim(28.5, 16)
+        ax.set_xlabel(r'${}-{}$'.format(filter1, filter2), fontsize=fsize)
+        ax.set_ylabel(r'${}$'.format(filter2), fontsize=fsize)
 
-    ax2.set_xlim(10.2, 6.8)
-    ax2.set_ylim(0, 0.25)
+    axs[2].set_xlim(10.2, 6.8)
+    axs[2].set_ylim(0, 0.25)
 
-    ax1.set_xlabel(r'${}-{}$'.format(filter1, filter2), fontsize=fsize)
-    ax1.set_ylabel(r'${}$'.format(filter2), fontsize=fsize)
+    #axs[3].set_xlim(0, 6.8)
+    #axs[3].set_ylim(0, 0.25)
 
-    if xlabel is None:
-        ax2.set_xlabel(r'$\log \rm{Age}\ (\rm{yr})$', fontsize=fsize)
-    ax2.set_ylabel(r'$\rm{Relative\ Mass\ Formed}$', fontsize=fsize)
+    axs[2].set_xlabel(r'$\log \rm{Age}\ (\rm{yr})$', fontsize=fsize)
+    axs[2].set_ylabel(r'$\rm{Relative\ Mass\ Formed}$', fontsize=fsize)
+    axs[3].set_xlabel(r'$\rm{Evolutionary\ Stage}$', fontsize=fsize)
     sns.despine()
     sns.color_palette("bright")
     sns.set_context("talk")
     sns.set_style("whitegrid")
     sns.set_style("ticks")
-    [ax.grid() for ax in (ax1, ax2)]
-    return fig, (ax1, ax2)
+    [ax.grid() for ax in axs]
+    return fig, axs
 
 
 def main(argv):
@@ -75,11 +79,8 @@ def main(argv):
     parser.add_argument('-o', '--outfile', type=str, default="cmd_animate.mp4",
                         help='movie name')
 
-    parser.add_argument('-d', '--ds', type=float, default=0.1,
-                        help='step size')
-
-    parser.add_argument('-z', '--zcol', type=str, default='logAge',
-                        help='animate by column')
+    parser.add_argument('-d', '--dlage', type=float, default=0.1,
+                        help='lage  step size')
 
     args = parser.parse_args(argv)
 
@@ -89,31 +90,33 @@ def main(argv):
     sgal, lfd = read_data(args.infile, args.lffile)
     filter1 = args.infile.split('_')[2].upper()
     filter2 = args.infile.split('_')[3].upper()
-    zdata, mass, color, mag = load_data(sgal, filter1, filter2, lfd=lfd,
-                                       zcol=args.zcol)
+    lage, mass, color, mag, stage = load_data(sgal, filter1, filter2, lfd=lfd)
     totmass = np.sum(mass)
-    if args.zcol == 'logAge':
-        zdatas = np.arange(6.6, 10.21, args.ds)[::-1]
-        xlabel = None
-    if args.zcol == 'stage':
-        #import pdb; pdb.set_trace()
-        zdatas = np.unique(zdata)[::-1]
-        xlabel = r'$\rm{Evolutionary\ Stage}$'
-    inds = [list(set(np.nonzero(zdata < zdatas[i])[0]) &
-                 set(np.nonzero(zdata >= zdatas[i+1])[0]))
-            for i in range(len(zdatas)-1)]
+
+    lages = np.arange(6.6, 10.21, args.dlage)[::-1]
+    #import pdb; pdb.set_trace()
+    stagess = np.unique(stage)[::-1]
+    inds = [list(set(np.nonzero(lage < lages[i])[0]) &
+                 set(np.nonzero(lage >= lages[i+1])[0]))
+            for i in range(len(lages)-1)]
     smass =  [np.sum(mass[i]) / totmass for i in inds]
 
-    fig, (ax1, ax2) = plot_setup(filter1, filter2, xlabel=xlabel)
-    ax2.plot(zdatas[:-1], smass, linestyle='steps-mid', color='k', lw=2)
+    fig, axs = plot_setup(filter1, filter2)
+    axs[2].plot(lages[:-1], smass, linestyle='steps-mid', color='k', lw=2)
 
     ii = np.array([])
     with writer.saving(fig, args.outfile, 300):
-        for i in range(len(zdatas) - 1):
-            ax1.plot(color[inds[i]], mag[inds[i]], '.')
-            ax2.plot(zdatas[i], smass[i], 'o')
+        for i in range(len(lages) - 1):
+            axs[0].plot(color[inds[i]], mag[inds[i]], '.')
+            axs[1].scatter(color[inds[i]], mag[inds[i]], c=stage[inds[i]],
+                           marker='o', s=15,
+                           cmap=plt.cm.Spectral)
+            # add a cmd plot colored by stage for same age binning
+            # evolutionary stage histogram
+            axs[2].plot(lages[i], smass[i], 'o')
+            #axs[3].plot(lages[i], smass[i], 'o')
             writer.grab_frame()
-        ax2.plot(zdatas[i], smass[i], 'o')
+        axs[2].plot(lages[i], smass[i], 'o')
         writer.grab_frame()
         writer.grab_frame()
     print('wrote {}'.format(args.outfile))
